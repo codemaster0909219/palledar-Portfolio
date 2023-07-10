b9d8b56a456b5405682c3e2d46bf257494966e11
crveskig <crveskig@gmail.com>
2023-07-10 10:58:05 +0530

    Added tech texts

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -10,6 +10,7 @@ const Tech = () => {
       {technologies.map((technology)=>(
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas  icon={technology.icon}/>
+          <p className='text-center my-3'>{technology.name}</p>
         </div>
       ))}
     </div>
